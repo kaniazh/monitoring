6938ed6e099524120da00c4d36426439aa16947b
kaniazh <kania.z.h@gmail.com>
2021-09-21 12:21:53 +0700

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -230,43 +230,7 @@
                     Simpan Profil</button>
             </div>
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="box box-primary">
-                    <div class="box-body table-responsive">
-                        <table class="table table-bordered table-striped table-hover datatable">
-                            <thead>
-                                <tr>
-                                    <th width="5">NO</th>
-                                    <th>KELAS</th>
-                                    <th>NIS</th>
-                                    <th>NISN</th>
-                                    <th>NAMA LENGKAP</th>
-                                    <th>TTL</th>
-                                    <th>JK</th>
-                                    <th>ALAMAT</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $n = 1;
-                                foreach ($students as $row) : ?>
-                                    <tr>
-                                        <td><?= $n++ . '.'; ?></td>
-                                        <td><?= $row->kelas_kd; ?></td>
-                                        <td><?= $row->nis; ?></td>
-                                        <td><?= $row->nisn; ?></td>
-                                        <td><?= $row->nama; ?></td>
-                                        <td><?= $row->tmp_lhr . ', ' . date('d M Y', strtotime($row->tgl_lhr)); ?></td>
-                                        <td><?= $row->jk == 'L' ? 'Laki-Laki' : 'Perempuan'; ?></td>
-                                        <td><?= $row->alamat; ?></td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </section>
+            
             <?php
             $db_host='localhost';
             $db_database='e_raport';
